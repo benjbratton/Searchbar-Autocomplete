b14732c68690c2a75fc0a1e812c66eac81816378
caglen2 <36609665+caglen2@users.noreply.github.com>
2018-02-18 17:51:19 -0500

    Update Autocomplete.java

--- Autocomplete.java
+++ Autocomplete.java
@@ -1,4 +1,9 @@
 
-public class Autocomplete {
+public class Autocomplete {// implement sorting algorithm in this class
+ /* Initializes the data structure from the given array of terms. */
+ public Autocomplete(Term[] terms)
+ /* Returns all terms that start with the given prefix, in descending
+order of weight. */
+ public Term[] allMatches(String prefix)
 
 }
