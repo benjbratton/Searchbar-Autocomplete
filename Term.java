33beccb88a9abeab1a0346cb43d1b65a2c7b52f3
caglen2 <36609665+caglen2@users.noreply.github.com>
2018-03-07 15:41:09 -0500

    Update Term.java

--- Term.java
+++ Term.java
@@ -1,27 +1,35 @@
 public class Term implements Comparable<Term> {
- /* Initializes a term with the given query string and weight. */
- public Term(String query, long weight){
-  if(query.equals(null))
-   throw new java.lang.NullPointerException();
-  if(weight < 0)
-   throw new java.lang.IllegalArgumentException();
- }
- /* Compares the two terms in descending order by weight. */
- public static Comparator<Term> byReverseWeightOrder(){
-  
- }
- /* Compares the two terms in lexicographic order but using only the first
-r characters of each query. */
- public static Comparator<Term> byPrefixOrder(int r){
-  if (r < 0) throw new java.lang.IllegalArgumentException();
- }
- /* Compares the two terms in lexicographic order by query. */
- public int compareTo(Term that){
-  
- }
- // Returns a string representation of this term in the following format:
- // weight (i.e., ??.toString()), followed by a tab, followed by query.
- public String toString(){
-  return weight.toString() + "    " + query;
- }
+	private final String word;
+	private final long worth;
+	/* Initializes a term with the given query string and weight. */
+	public Term(String query, long weight){
+		if(query.equals(null))
+		  throw new java.lang.NullPointerException();
+		if(weight < 0)
+		  throw new java.lang.IllegalArgumentException();
+		word = query;
+		worth = weight;
+	  
+	}
+	/* Compares the two terms in descending order by weight. */
+	public static Comparator<Term> byReverseWeightOrder(){
+		
+	}
+	/* Compares the two terms in lexicographic order but using only the first
+	r characters of each query. */
+	public static Comparator<Term> byPrefixOrder(int r){
+		if(r < 0)
+			throw new java.lang.IllegalArgumentException();
+		
+		
+	}
+	/* Compares the two terms in lexicographic order by query. */
+	public int compareTo(Term that){
+		
+	}
+	// Returns a string representation of this term in the following format:
+	// weight (i.e., ??.toString()), followed by a tab, followed by query.
+	public String toString(){
+		return Long.toString(worth) + "	" + word;
+	}
 }
