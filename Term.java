0f5e8f4f0d3a4e148c76bb4b7fef685e0548ac97
caglen2 <36609665+caglen2@users.noreply.github.com>
2018-03-07 15:25:12 -0500

    Update Term.java

--- Term.java
+++ Term.java
@@ -1,8 +1,10 @@
 public class Term implements Comparable<Term> {
  /* Initializes a term with the given query string and weight. */
  public Term(String query, long weight){
-  query = "";
-  weight = 0;
+  if(query.equals(null))
+   throw new java.lang.NullPointerException();
+  if(weight < 0)
+   throw new java.lang.IllegalArgumentException();
  }
  /* Compares the two terms in descending order by weight. */
  public static Comparator<Term> byReverseWeightOrder(){
@@ -11,7 +13,7 @@ public class Term implements Comparable<Term> {
  /* Compares the two terms in lexicographic order but using only the first
 r characters of each query. */
  public static Comparator<Term> byPrefixOrder(int r){
-  
+  throw new java.lang.IllegalArgumentException();
  }
  /* Compares the two terms in lexicographic order by query. */
  public int compareTo(Term that){
@@ -20,6 +22,6 @@ r characters of each query. */
  // Returns a string representation of this term in the following format:
  // weight (i.e., ??.toString()), followed by a tab, followed by query.
  public String toString(){
-  
+  return weight.toString() + "    " + query;
  }
 }
