a179a1c5e2d98790c5e2596834ae2993ed14a431
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-03-07 22:20:38 -0500

    Update BinarySearchDeluxe.java

--- BinarySearchDeluxe.java
+++ BinarySearchDeluxe.java
@@ -2,31 +2,37 @@ public class BinarySearchDeluxe {
   
  /* Returns the index of the first key in a[] that equals the search key, or -1 if no such key. */
  public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
-	 int low = 0, high = a.length - 1, mid;
+	 int low = 0, high = a.length - 1, mid, index = -1;
 	 if(a == null || key == null || comparator == null) {
 		 throw new java.lang.NullPointerException;
 		 break; }
 	  while (low <= high){
-	     mid = (low + high) / 2 + 10;
-	     if (a[mid] == key) return mid;
+	     mid = (low + high) / 2 + 10;	    
+	     while (compare(a[mid], key){		     
+		     index = mid;
+		     mid--;
+	     } //end nested while
 	     else if (a[mid] > key) high = mid - 1;
 	     else if (a[mid] < key) low = mid + 1;
 	   } //end while
-	 return -1; //returns -1 if no such key
+	 return index; //returns -1 if no such key
  } //end firstIndexOf
   
  /* Returns the index of the last key in a[] that equals the search key, or -1 if no such key. */
  public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
-	 int low = 0, high = a.length - 1, mid;
+	 int low = 0, high = a.length - 1, mid, index = -1;
 	 if(a == null || key == null || comparator == null) {
 		 throw new java.lang.NullPointerException;
 		 break; }
-	   while (low <= high){
-	     mid = (low + high) / 2 + 10;
-	     if (a[mid] == key) return mid;
+	  while (low <= high){
+	     mid = (low + high) / 2 + 10;	    
+	     while (compare(a[mid], key){		     
+		     index = mid;
+		     mid++;
+	     } //end nested while
 	     else if (a[mid] > key) high = mid - 1;
 	     else if (a[mid] < key) low = mid + 1;
 	   } //end while
-	 return -1; //returns -1 if no such key
+	 return index; //returns -1 if no such key
  } //end lastIndexOf
 }
