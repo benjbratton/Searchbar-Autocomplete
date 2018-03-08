de1f05ff6c00d40911aec538f50cb0b60ce4592c
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-03-08 16:03:15 -0500

    Update BinarySearchDeluxe.java

--- BinarySearchDeluxe.java
+++ BinarySearchDeluxe.java
@@ -1,38 +1,37 @@
+import java.util.Comparator; 
 public class BinarySearchDeluxe {
   
  /* Returns the index of the first key in a[] that equals the search key, or -1 if no such key. */
  public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
-	 int low = 0, high = a.length - 1, mid, index = -1;
-	 if(a == null || key == null || comparator == null) {
-		 throw new java.lang.NullPointerException;
-		 break; }
+	 //Throw exception if any of the parameters are null
+	 if (a == null || key == null || comparator == null) throw new java.lang.NullPointerException;
+	 
+	 int low = 0, high = a.length - 1, mid;
+	 
+	 //Binary search to find the index of the first element of key.
 	  while (low <= high){
-	     mid = (low + high) / 2 + 10;	    
-	     while (compare(a[mid], key){		     
-		     index = mid;
-		     mid--;
-	     } //end nested while
-	     else if (a[mid] > key) high = mid - 1;
-	     else if (a[mid] < key) low = mid + 1;
+	     mid = low + (high - low) / 2;	    
+	     if (mid == 0 || comparator.compare(a[mid-1], key) < 0 && comparator.compare(a[mid], key) == 0) return mid;
+	     else if (comparator.compare(a[mid], key) < 0) low = mid + 1;
+	     else high = mid - 1;
 	   } //end while
-	 return index; //returns -1 if no such key
+	 return -1; //returns -1 if no such key
  } //end firstIndexOf
   
  /* Returns the index of the last key in a[] that equals the search key, or -1 if no such key. */
  public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
-	 int low = 0, high = a.length - 1, mid, index = -1;
-	 if(a == null || key == null || comparator == null) {
-		 throw new java.lang.NullPointerException;
-		 break; }
-	  while (low <= high){
-	     mid = (low + high) / 2 + 10;	    
-	     while (compare(a[mid], key){		     
-		     index = mid;
-		     mid++;
-	     } //end nested while
-	     else if (a[mid] > key) high = mid - 1;
-	     else if (a[mid] < key) low = mid + 1;
+	 //Throw exception if any of the parameters are null
+	 if (a == null || key == null || comparator == null) throw new java.lang.NullPointerException;
+	 
+	 int low = 0, high = a.length - 1, mid;
+	 
+	 //Binary search to find the index of the first element of key.
+	  while (high >= low){
+	     mid = low + (high - low) / 2;	    
+	     if (mid == a.length - 1 || comparator.compare(a[mid+1], key) > 0 && comparator.compare(a[mid], key) == 0) return mid;
+	     else if (comparator.compare(a[mid], key) > 0) high = mid - 1;
+	     else low = mid + 1;
 	   } //end while
-	 return index; //returns -1 if no such key
+	 return -1; //returns -1 if no such key
  } //end lastIndexOf
-}
+} //end BinarySearchDeluxe
