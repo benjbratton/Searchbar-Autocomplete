d55fe82c43f19c3f8365daa5f3cc7890d2d29f3c
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-03-09 16:37:45 -0500

    Update Autocomplete.java

--- Autocomplete.java
+++ Autocomplete.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
 
 public class Autocomplete {// implement sorting algorithm in this class
+	
+	public static void main(String[] args) {
+		 // always print messages on screen when debugging
+		 // System.out.println(…);
+		 // read in the terms from a file
+		 String filename = args[0]; // first argument from command line
+		 In in = new In(filename);
+		 int N = in.readInt();
+		 Term[] terms = new Term[N];
+		 for (int i = 0; i < N; i++) {
+		 long weight = in.readLong(); // read the next weight
+		 in.readChar(); // scan past the tab
+		 String query = in.readLine(); // read the next query
+		 terms[i] = new Term(query, weight); // construct the term
+		 }
+		 // read in queries from standard input and print the top k matching terms
+		 int k = Integer.parseInt(args[1]); // 2nd argument from command line
+		 Autocomplete autocomplete = new Autocomplete(terms);
+		 while (StdIn.hasNextLine()) {
+		 String prefix = StdIn.readLine();
+		 Term[] results = autocomplete.allMatches(prefix);
+		 for (int i = 0; i < Math.min(k, results.length); i++)
+		 System.out.println(results[i]);
+		 }
+		} 
+	
 	public Term[] terms;
 	/* Initializes the data structure from the given array of terms. */
 	public Autocomplete(Term[] terms){
@@ -8,42 +35,16 @@ public class Autocomplete {// implement sorting algorithm in this class
 	/* Returns all terms that start with the given prefix, in descending
 	order of weight. */
 	public Term[] allMatches(String prefix){
-		 int first, last;
-		 first = BinarySearchDeluxe.firstIndexOf(terms, prefix, Comparator<prefix> comp);
-		 last = BinarySearchDeluxe.lastIndexOf(terms, prefix, Comparator<prefix> comp);
-		 Term[] temp = new Term[last-first];
-		 for(int i = first; i <= last; i++) {
-			 temp[i] = terms[i];
-			 System.out.println(temp[i]);
-		 }
+		 int first, last, r = prefix.length();
+		 Term word = new Term(prefix, 0);
+		 first = BinarySearchDeluxe.firstIndexOf(terms, word, Term.byPrefixOrder(r));
+		 last = BinarySearchDeluxe.lastIndexOf(terms, word, Term.byPrefixOrder(r));
+		 Term[] temp = new Term[last-first]; //Create an array with only the works with the key in them
+		 for(int i = 0; i < temp.length; i++) {
+			 temp[i] = terms[first + i];
+		 	}
+		 Arrays.sort(temp, Term.byReverseWeightOrder()); //Sort the array by lexicographic and weight order
+		 return temp;
+
 	}
-	public static void sort(Comparable[] a) {
-		 int N = a.length;
-		 Comparable[] aux = new Comparable[N];
-		 for(int sz = 1; sz < N; sz=sz+sz) {
-			 for(int lo = 0; lo < N-sz; lo += sz+sz) {
-				 merge(a, aux, lo, lo+sz-1, Math.min(lo+sz+sz-1, N-1));
-			 }
-		 }
-	}
-	 private static void merge(Comparable[]a, Comparable[] aux, int lo, int mid, int hi) {
-		 Comparator<Term> comparator;
-		 for(int k = lo; k<=hi; k++) {
-			 aux[k] = a[k];
-		 }
-		 int i = lo;
-		 int j = mid+1;
-		 for(int k=lo; k<=hi;k++) {
-			 if(i>mid)break;
-			 else if(j>hi) {
-				 a[k] = aux[i];
-				 i++;}
-			 else if(comparator.compare(a[i], a[j])==1){
-				 a[k]=aux[j];
-				 j++;}
-			 else {
-				 a[k] = aux[i];
-				 i++;}
-		 }
-	 }
-}
+}//End Autoomplete
