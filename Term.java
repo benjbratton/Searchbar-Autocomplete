ded62129c2ace138f6e28e55ce7c7304cf743603
caglen2 <36609665+caglen2@users.noreply.github.com>
2018-03-09 15:35:56 -0500

    Update Term.java

--- Term.java
+++ Term.java
@@ -1,44 +1,51 @@
+import java.util.Comparator;
+
 public class Term implements Comparable<Term> {
 	public String word;
 	public long worth;
 	/* Initializes a term with the given query string and weight. */
 	public Term(String query, long weight){
 		if(query.equals(null))
-		  throw new java.lang.NullPointerException();
+		  throw new java.lang.NullPointerException();//checks if the query is empty
 		if(weight < 0)
-		  throw new java.lang.IllegalArgumentException();
-		word = query;
-		worth = weight;
+		  throw new java.lang.IllegalArgumentException();//checks if the weight is empty
+		word = query;//creates a variable that can be used by all methods
+		worth = weight;//creates a variable that can be used by all methods
 	  
 	}
 	/* Compares the two terms in descending order by weight. */
 	public static Comparator<Term> byReverseWeightOrder(){
-		return new ReverseOrder();
+		return new ReverseOrder();//returns a new class that compares two terms
 	}
 	public static class ReverseOrder implements Comparator<Term>{
-		public long compare(Term v, Term w) {
-			return v.worth - w.worth;
+		public int compare(Term v, Term w) {//compares the two terms based on their weight
+			if(v.worth > w.worth)
+				return -1;//returns the comparison
+			else if(v.worth < w.worth)
+				return 1;//returns the comparison
+			else
+				return 0;//returns the comparison
 		}
 	}
 	/* Compares the two terms in lexicographic order but using only the first
 	r characters of each query. */
 	public static Comparator<Term> byPrefixOrder(int r){
 		if(r < 0)
-			throw new java.lang.IllegalArgumentException();
-		return new PrefixOrder(r);	
+			throw new java.lang.IllegalArgumentException();//checks if the given input is negative and throws an exception if it is
+		return new PrefixOrder(r);//returns a new class that compares two terms	
 	}
 	public static class PrefixOrder implements Comparator<Term> {
-		int r;
-		PrefixOrder(int r){
+		int r;//int r from byPreficOrder
+		PrefixOrder(int r){//constructor of PrefixOrder
 			this.r = r;
 		}
-		public int compare(Term v, Term w) {
-			return v.compareTo(w);
+		public int compare(Term v, Term w) {//compares the two terms based on the prefix of length r
+			return v.compareTo(w);//returns the comparison
 		}
 	}
 	/* Compares the two terms in lexicographic order by query. */
 	public int compareTo(Term that){
-		return this.word.compareTo(that.word);
+		return this.word.compareTo(that.word);//compares the current query with the next query
 	}
 	// Returns a string representation of this term in the following format:
 	// weight (i.e., ??.toString()), followed by a tab, followed by query.
@@ -46,3 +53,4 @@ public class Term implements Comparable<Term> {
 		return Long.toString(this.worth) + "\t" + this.word;
 	}
 }
+
