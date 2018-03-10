1d2c88af3c49b1d816743233fa9ef1cc10950699
Ben Bratton <36572589+benjbratton@users.noreply.github.com>
2018-03-09 21:58:25 -0500

    Update Autocomplete.java

--- Autocomplete.java
+++ Autocomplete.java
@@ -1,3 +1,13 @@
+// Course:  CPS 350
+// Semester: 2018 Spring
+// Name: Benjamin Bratton and Nick Cagle
+// Section: 01
+// Assignment: 03
+// Purpose: This program is designed to guess what the user is trying to type in a search engine.
+//			Based on a database of past searches it will suggest the top 10 (or less) likely
+//			recomendations for the user to select.
+// Date completed: March 9, 2018
+
 import java.util.Arrays;
 
 public class Autocomplete {// implement sorting algorithm in this class
@@ -5,7 +15,7 @@ public class Autocomplete {// implement sorting algorithm in this class
 	public static void main(String[] args) {
 		 // always print messages on screen when debugging
 		 // System.out.println(…);
-		 // read in the terms from a file
+		 // read in the terms from a file				
 		 String filename = args[0]; // first argument from command line
 		 In in = new In(filename);
 		 int N = in.readInt();
@@ -16,6 +26,7 @@ public class Autocomplete {// implement sorting algorithm in this class
 		 String query = in.readLine(); // read the next query
 		 terms[i] = new Term(query, weight); // construct the term
 		 }
+		 
 		 // read in queries from standard input and print the top k matching terms
 		 int k = Integer.parseInt(args[1]); // 2nd argument from command line
 		 Autocomplete autocomplete = new Autocomplete(terms);
@@ -25,7 +36,7 @@ public class Autocomplete {// implement sorting algorithm in this class
 		 for (int i = 0; i < Math.min(k, results.length); i++)
 		 System.out.println(results[i]);
 		 }
-		} 
+		}
 	
 	public Term[] terms;
 	/* Initializes the data structure from the given array of terms. */
@@ -36,15 +47,26 @@ public class Autocomplete {// implement sorting algorithm in this class
 	order of weight. */
 	public Term[] allMatches(String prefix){
 		 int first, last, r = prefix.length();
+		 
+		 //create an array with all the terms
 		 Term word = new Term(prefix, 0);
+	 
 		 first = BinarySearchDeluxe.firstIndexOf(terms, word, Term.byPrefixOrder(r));
+
 		 last = BinarySearchDeluxe.lastIndexOf(terms, word, Term.byPrefixOrder(r));
-		 Term[] temp = new Term[last-first]; //Create an array with only the works with the key in them
-		 for(int i = 0; i < temp.length; i++) {
-			 temp[i] = terms[first + i];
+		 
+		 //Catch if array is empty
+		 if(first == -1 || last == -1) {
+			 Term[] nothing = new Term[0];
+			 return nothing;
+		 }
+		 
+		 Term[] temp = new Term[last - first + 1]; //Create an array with only the works with the key in them
+		 for(int i = first; i <= last; i++) {
+			 temp[i - first] = terms[i];
 		 	}
 		 Arrays.sort(temp, Term.byReverseWeightOrder()); //Sort the array by lexicographic and weight order
 		 return temp;
 
 	}
-}//End Autoomplete
+}//End Autocomplete	
